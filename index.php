b15abed892c34fc4f328c402203d846e6e98b075
So close <soclose55355@gmail.com>
2023-04-18 16:50:48 +0300

    taskfix

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ class Lutra extends Animals {
 
     public function Speak()
     {
-        echo "I`m Lutra, my name is ". $this->getName().", and my color is ".$this->getColor()."!"."<br>";
+        echo "I`m Lutra, my name is ". $this->getName().", and my color is ".$this->getColor()."!Unestimated view, entered to the Chervonoy Book of Ukraine.<br> In Ukraine, it is found on the entire territory of the country, except for the Krym."."<br>";
     }
 }
 
